899cbd9b701b181765cfdc5479362e8bb24c0f5e
Elsti97 <elsti.shehati@hotmail.it>
2022-11-23 16:07:32 +0100

    Header done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
+
+    <!-- * HEADER -->
     <HeaderComp/>
+
+    <!--* MAIN -->
     <MainComp/>
+
+    <!--* FOOTER -->
     <FooterComp/>
   </div>
 </template>
@@ -24,10 +30,10 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
 }
 </style>
--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-
+  <div class="between">
+    <img src="../assets/img/dc-logo.png" alt="Logo header">
+    <div>
+      <ul>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -9,3 +24,23 @@ export default {
   name: 'HeaderComp',
 }
 </script>
+
+<style scoped lang="scss">
+*{
+  display: flex;
+}
+
+.between{
+  justify-content: space-around;
+  width: 80%;
+  margin: auto;
+}
+
+ul{
+  align-self: center;
+}
+
+li{
+  padding: .5rem;
+}
+</style>
